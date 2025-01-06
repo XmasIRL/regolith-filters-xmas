b1d4cb50f7962113e1b73ed67980798042ec76d3
Xmas <github@xmas.social>
2025-01-07 00:27:06 +0530

    Fix exaroton_deploy

--- exaroton_deploy/index.js
+++ exaroton_deploy/index.js
@@ -7,22 +7,25 @@ const { asyncFolderWalker } = require('async-folder-walker')
 
 const settings = process.argv[2] ? JSON.parse(process.argv[2]) : null;
 
+const keepExistingPacks = settings?.keepExistingPacks ?? true;
+const keepExistingWorlds = settings?.keepExistingWorlds ?? true;
+
 const envPath = settings?.envPath ?? 'data/exaroton_deploy/.env';
 dotenv.config({ path: envPath });
 const token = process.env.TOKEN;
 const serverID = process.env.SERVER_ID;
 
 const ROOT_DIR = process.env.ROOT_DIR + "/"
-const files_path = ROOT_DIR +settings?.filesPath ?? 'data/exaroton_deploy/files';
+const filesPath  = ROOT_DIR +settings?.filesPath  ?? 'packs/data/exaroton_deploy/files';
+const packsPath  = ROOT_DIR +settings?.packs_path ?? 'packs/data/exaroton_deploy/packs';
+const worldsPath = ROOT_DIR +settings?.filesPath  ?? 'packs/data/exaroton_deploy/worlds';
 
 if (!token || !serverID) {
   console.error("Error: Invalid configuration. Please check whether your .env file has TOKEN and SERVER_ID defined.");
   process.exit(1);
 }
 
-async function deployFiles() {
-
-    console.log('Deploying server files (Modify only) ...');
+async function deploy() {
 
     try {
 
@@ -30,27 +33,63 @@ async function deployFiles() {
         const server = client.server(serverID);
         await server.get();
 
-
         // recursive read server files
-        async function readFiles(filePath) {
-            const fileRemote = server.getFile(filePath);
-            const children = await fileRemote.getChildren();
-            for (const child of children) {
-                if (!child.isDirectory) console.log(child.path);
-                else try { await readFiles(child.path) } catch (e) {}
-            }
-        }
-
+        // async function readFiles(filePath) {
+        //     const fileRemote = server.getFile(filePath);
+        //     const children = await fileRemote.getChildren();
+        //     for (const child of children) {
+        //         if (!child.isDirectory) console.log(child.path);
+        //         else try { await readFiles(child.path) } catch (e) {}
+        //     }
+        // }
         // await readFiles('');
 
-        const walker = asyncFolderWalker(files_path,{statFilter: st => !st.isDirectory()});
-        for await (const filePath of walker) {
-            const destPath = path.relative(files_path, filePath).replace(/\\/g, '/');
+        // deploy files
+        console.log('Deploying server files... ');
+        console.log('Existing files will be kept.');
+        let filesWalker = asyncFolderWalker(filesPath,{statFilter: st => !st.isDirectory()});
+        for await (const filePath of filesWalker) {
+            const destPath = path.relative(filesPath, filePath).replace(/\\/g, '/');
             console.log('Deploying ' + destPath);
-
             const fileRemote = server.getFile(destPath);
             // try {await fileRemote.delete();} catch (e) { console.error(e.message); }
             try {await fileRemote.uploadFromStream(fs.createReadStream(filePath));} catch (e) { console.error(e.message); }
+        }
+
+        // deploy packs
+        console.log('Deploying server packs... ');
+        console.log(keepExistingPacks ? 'Existing packs will be kept.' : 'Existing packs will be deleted.');
+        if (!fs.existsSync(packsPath)) { console.error('Packs folder not found!'); }
+        else {
+          if (!keepExistingPacks) for (const child of await server.getFile('packs').getChildren()) {try {
+                console.log('Deleting ' + child.path);
+                await fileRemote.delete();
+          } catch (e) { console.error(e.message); }}
+          let packsWalker = asyncFolderWalker(packsPath,{statFilter: st => !st.isDirectory()});
+          for await (const filePath of packsWalker) {
+              const destPath = 'packs/' + path.basename(filePath);
+              console.log('Deploying ' + destPath);
+              const fileRemote = server.getFile(destPath);
+              try {await fileRemote.uploadFromStream(fs.createReadStream(filePath));} catch (e) { console.error(e.message); }
+          }
+        }
+
+        // deploy worlds
+        console.log('Deploying server worlds... ');
+        console.log(keepExistingWorlds ? 'Existing worlds will be kept.' : 'Existing worlds will be deleted.');
+        if (!fs.existsSync(worldsPath)) { console.error('Worlds folder not found!'); }
+        else {
+          if (!keepExistingWorlds) for (const child of await server.getFile('worlds').getChildren()) {try {
+                console.log('Deleting ' + child.path);
+                await fileRemote.delete();
+          } catch (e) { console.error(e.message); }}
+          let worldsWalker = asyncFolderWalker(worldsPath,{statFilter: st => !st.isDirectory()});
+          for await (const filePath of worldsWalker) {
+              const destPath = 'worlds/' + path.basename(filePath);
+              console.log('Deploying ' + destPath);
+              const fileRemote = server.getFile(destPath);
+              try {await fileRemote.uploadFromStream(fs.createReadStream(filePath));} catch (e) { console.error(e.message); }
+          }
         }
 
         // if(server.hasStatus(server.STATUS.ONLINE)) await server.restart();
@@ -62,4 +101,4 @@ async function deployFiles() {
 
 }
 
-deployFiles();
+deploy();
